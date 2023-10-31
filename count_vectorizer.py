4de55b422a4227e3c705d1e3c9810420301eaee5
Александра Истомина <alexseverina@sirius.online>
2023-10-31 15:38:05 +0300

    Added TfidfTransformer and TfidfVectorizer classes

--- count_vectorizer.py
+++ count_vectorizer.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -52,12 +53,87 @@ class CountVectorizer:
         return self.feature_names
 
 
+class TfidfTransformer:
+    """
+    Class that allows to calculate tf-idf metric for words in a cluster
+    of texts based on texts count matrix, i.e. every given text is
+    represented by a list of word counts of words.
+    """
+
+    @staticmethod
+    def tf_transform(cnt_matrix: list[list]) -> list[list]:
+        """
+        Calculates tf metric for words by given count matrix.
+
+        :param cnt_matrix: texts in form of matrix with word counts.
+        :return: tf metric for every word in every text
+        """
+        return [[round(x / sum(row), 3) for x in row] for row in cnt_matrix]
+
+    @staticmethod
+    def idf_transform(cnt_matrix: list[list]) -> list[float]:
+        """
+        Calculates idf metric for words by given count matrix.
+
+        :param cnt_matrix: texts in form of matrix with word counts.
+        :return: idf metric for every word in every text
+        """
+        doc_num = len(cnt_matrix)
+        word_num = len(cnt_matrix[0])
+
+        docs_with_word = [[1 if x > 0 else 0 for x in row] for row in cnt_matrix]
+        words_cnts = [sum([row[i] for row in docs_with_word]) for i in range(word_num)]
+        return [
+            round(math.log((doc_num + 1) / (word_cnt + 1)) + 1, 3)
+            for word_cnt in words_cnts
+        ]
+
+    def fit_transform(self, cnt_matrix: list[list]) -> list[list]:
+        """
+        Calculates tf-idf metric for words by given count matrix.
+
+        :param cnt_matrix: texts in form of matrix with word counts.
+        :return: tf-idf metric for every word in every text
+        """
+        tf_matrix = self.tf_transform(cnt_matrix)
+        idf_matrix = self.idf_transform(cnt_matrix)
+        return [
+            [round(tf * idf, 3) for tf, idf in zip(row, idf_matrix)]
+            for row in tf_matrix
+        ]
+
+
+class TfidfVectorizer(CountVectorizer):
+    """
+    Class that allows to calculate tf-idf metric for words in a cluster of
+    texts. Words are stored in lexicographically ordered feature_names list.
+    Every text can be obtained in a form of a list of word counts of words
+    from feature_names.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.transformer = TfidfTransformer()
+
+    def fit_transform(self, texts: list[str]) -> list[list]:
+        """
+        Builds by the given corpus of texts list of present words and
+        the document-term matrix and tf-idf matrix.
+
+        :param texts: list of strings to analyze
+        :return: tf-idf matrix of texts
+        """
+        super().fit_transform(texts)
+        return self.transformer.fit_transform(self.dt_matrix)
+
+
 if __name__ == "__main__":
     corpus = [
         "Crock Pot Pasta Never boil pasta again",
         "Pasta Pomodoro Fresh ingredients Parmesan to taste",
     ]
-    vectorizer = CountVectorizer()
+
+    vectorizer = TfidfVectorizer()
     count_matrix = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
     print(count_matrix)
